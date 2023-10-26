2f754ad1e23f5cb2b7cda4b30892bc805f705783
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-10-26 16:47:29 +0530

    created the work with us section

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import SelfImprovement from "@/components/SelfImprovement";
 import BeBestYou from "../components/BeBestYou";
 import EverWonder from "../components/EverWonder";
 import Test from "../components/Test";
+import WorkWithUs from "../components/WorkWithUs";
 
 
 
@@ -25,6 +26,9 @@ export default function Home() {
       <BeBestYou />
       <EverWonder />
       <Test />
+      <WorkWithUs />
+
+
 
 
 
